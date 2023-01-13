601acb9195b4dcc90fe99d436117f3adcaafef6d
urielmajb <urielmajb@gmail.com>
2023-01-13 10:09:10 -0600

    Depurar errores en sitio de wordpress

---
+++ debugging.php
@@ -0,0 +1,12 @@
+<?php
+
+//Hacer una copia del wp-config.php antes de colocar en el siguiente codigo.
+//Verificar de manera local
+
+//activa el modo de “depuración” en todo WordPress
+define( 'WP_DEBUG', true ); //Genera un log en wp-contets/debug.log
+
+define( 'WP_DEBUG_LOG', true );
+define('WP_DEBUG_DISPLAY', false );
+
+?>
